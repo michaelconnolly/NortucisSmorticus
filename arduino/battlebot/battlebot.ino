91a51a083bf17521a732a79d32d550915e2faa81
Brian <brian@connolly.net>
2020-01-28 00:28:01 -0800

    New battlebot proto
    Got the OLED, motor and bluetooth working

---
+++ arduino/battlebot/battlebot.ino
@@ -0,0 +1,345 @@
+// Import external libraries
+#include <Adafruit_ssd1306syp.h>
+#include "SoftwareSerial.h"
+#include "WString.h"
+
+// Constants: I/O Pins
+#define PIN_MOTOR_A_ENABLE 5
+#define PIN_MOTOR_B_ENABLE 6
+#define PIN_MOTOR_A_INPUT1 7
+#define PIN_MOTOR_A_INPUT2 8
+#define PIN_MOTOR_B_INPUT1 9
+#define PIN_MOTOR_B_INPUT2 4
+#define PIN_LED            10
+#define PIN_I2C_SDA        A4
+#define PIN_I2C_SCL        A5
+
+// More Constants
+#define AUTO_SHUTOFF_TIME 30000
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// The various states of our bluetooth connection.
+enum BluetoothState {
+  BLUETOOTH_DISCONNECTED,
+  BLUETOOTH_CONNECTED,
+  BLUETOOTH_ABANDONDED
+};
+
+// Global Variables: Run state
+boolean dead = false;
+bool autoShutOff = false;
+unsigned long startTime = 0; 
+
+// Global Variables: the OLED Display, connected via I2C interface
+Adafruit_ssd1306syp display(PIN_I2C_SDA, PIN_I2C_SCL);
+
+// Global Variables: motor control
+int velocity = 100;  
+
+// Global Variables: Bluetooth command Queue
+SoftwareSerial bluetooth(2, 3);
+BluetoothState bluetoothState = BLUETOOTH_DISCONNECTED;
+char command = 'S';
+char prevCommand = 'A';
+unsigned long timeLastCommand = 0;  //Stores the time when the last command was received from the phone
+
+/*** DISPLAY ***/
+
+
+/**
+ * Main output for status while in main sequence. This is called once per loop.
+ */
+void displayStatus(String line1, String line2, String line3, String line4) {  
+  display.clear();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  display.setCursor(0,2);
+  display.println(line1);
+  
+  display.setCursor(0,18);
+  display.println(line2);
+    
+  display.setCursor(0,34);
+  display.println(line3);
+
+  display.setCursor(0,48);
+  display.println(line4);
+
+  display.update();
+}
+
+/**
+ * Used for error messages to the OLED display.
+ */
+void displayMessage(String line1, String line2) {
+  display.clear();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  
+  display.setCursor(0,2);
+  display.println(line1);
+  
+  display.setCursor(0,34);
+  display.println(line2);
+  
+  display.update();
+}
+
+
+/**
+ * Entrypoint: called once when the program first starts, just to initialize all the sub-components.
+ */
+void setup() {  
+  
+  // Record what time we started.
+  startTime = millis();
+  
+  // Init the serial line; important for debug messages back to the Arduino Serial Monitor, so you can plug
+  // the robot into the USB port, and get real time debug messages.
+  // Make sure you set the baudrate at 9600 in Serial Monitor as well.
+  Serial.begin(9600);
+  Serial.println("setup start...");
+
+  // Init motor control pins.
+  pinMode(PIN_MOTOR_A_INPUT1, OUTPUT);
+  pinMode(PIN_MOTOR_A_INPUT2, OUTPUT);
+  pinMode(PIN_MOTOR_B_INPUT1, OUTPUT);
+  pinMode(PIN_MOTOR_B_INPUT2, OUTPUT);
+  pinMode(PIN_MOTOR_A_ENABLE, OUTPUT);
+  pinMode(PIN_MOTOR_B_ENABLE, OUTPUT);
+
+  // Init LED pin, and initially set it to off.
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
+
+  // Init the serial pipe to the bluetooth receiver. NOTE: this can go faster by configuring the HC-05
+  // with an AT command. But is that necessary?
+  bluetooth.begin(9600);
+  
+  // Init the OLED display.
+  delay(1000);
+  display.initialize();
+
+  // Init the rest of our internal state.
+  dead = false;
+  Serial.println("setup end");
+}
+
+
+/**
+ * Main Loop: called over and over again as the robot runs, 
+ */
+void loop() {
+  
+  // If we have marked as dead, be dead and do nothing.
+  if (dead) { 
+    displayMessage("DEAD", "Deactivated due to auto-shutoff");
+    return;
+  }
+  
+  // Process the bluetooth command queue, which is all the commands from our remote control.
+  bluetoothProcess();
+  
+  // Update the LED screen with our current state.
+  bool connected = (bluetoothState == BLUETOOTH_CONNECTED);
+  int upSecs = (millis() - startTime) / 1000;
+  displayStatus(
+    connected ? F("CONNECTED") : F("DISCONNECTED"),
+    "runtime: " + String(upSecs), 
+    "cmd: " + String(command) + "/" + String(prevCommand) + "   v: " + String(velocity),
+    "objective: " + String("KILL"));
+  
+  // Engage auto-shutoff if it has been enabled.
+  if (autoShutOff && (millis() > (startTime + AUTO_SHUTOFF_TIME))) {
+    dead = true;
+    motorStop();
+    displayMessage("Auto-Stop", "Stopped due to shutdown timer");
+    delay(3000);
+  }
+}
+
+
+/** MOTOR CONTROL **/
+
+#define TURN_SCALER 0.40
+
+void motorForward() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_A_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity);
+  
+  digitalWrite(PIN_MOTOR_B_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_B_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity); 
+}
+
+void motorReverse() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_A_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity);
+    
+  digitalWrite(PIN_MOTOR_B_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_B_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity); 
+}
+
+void motorTurnLeft() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_A_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity);
+  
+  digitalWrite(PIN_MOTOR_B_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_B_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity); 
+}
+
+void motorTurnLeftForward() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_A_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity);
+  
+  digitalWrite(PIN_MOTOR_B_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_B_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity * TURN_SCALER); 
+}
+
+void motorTurnLeftReverse() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_A_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity);
+    
+  digitalWrite(PIN_MOTOR_B_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_B_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity * TURN_SCALER); 
+}
+
+void motorTurnRight() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_A_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity);
+  
+  digitalWrite(PIN_MOTOR_B_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_B_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity); 
+}
+
+void motorTurnRightForward() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_A_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity * TURN_SCALER);
+  
+  digitalWrite(PIN_MOTOR_B_INPUT1, HIGH);
+  digitalWrite(PIN_MOTOR_B_INPUT2, LOW);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity); 
+}
+
+void motorTurnRightReverse() {
+  digitalWrite(PIN_MOTOR_A_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_A_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_A_ENABLE, velocity * TURN_SCALER);
+    
+  digitalWrite(PIN_MOTOR_B_INPUT1, LOW);
+  digitalWrite(PIN_MOTOR_B_INPUT2, HIGH);
+  analogWrite(PIN_MOTOR_B_ENABLE, velocity); 
+}
+
+void motorStop() {
+  analogWrite(PIN_MOTOR_A_ENABLE, 0); 
+  analogWrite(PIN_MOTOR_B_ENABLE, 0); 
+}
+
+
+/**** BLUETOOTH ****/
+
+/**
+ * 
+ */
+void bluetoothProcess() {
+
+  // If no commands, then nothing to do.
+  if (bluetooth.available() <= 0) {
+    return;
+  }
+
+  // A little state management. Since we have data ready to read, that means we are connected.
+  bluetoothState = BLUETOOTH_CONNECTED;
+
+  // Record when the last command was processed.
+  timeLastCommand = millis();  
+
+  // Read the actual command from the bluetooth buffer.
+  prevCommand = command;
+  command = bluetooth.read();
+  if (command != 'S') {
+    Serial.println("cmd2new: " + String(command) + "/" + String(prevCommand));
+  }
+  
+  // Process the known commands. Note that we change pin mode only if new command is different from previous.  
+  if (command != prevCommand) {
+    Serial.println("cmd2: " + String(command));
+    switch (command){
+    case 'F': 
+      motorForward();
+      break;
+    case 'B': 
+      motorReverse();
+      break;
+    case 'L': 
+      motorTurnLeft();
+      break;
+    case 'R':
+      motorTurnRight();
+      break;
+    case 'S': 
+      motorStop();
+      break;
+    case 'I':
+      motorTurnRightForward();
+      break;
+    case 'J':
+     motorTurnRightReverse();
+      break;       
+    case 'G':
+      motorTurnLeftForward();
+      break;
+    case 'H':
+      motorTurnLeftReverse();
+      break;
+    case 'W':  
+      //Front Lights ON
+      digitalWrite(PIN_LED, HIGH);
+      break;
+    case 'w':  
+      //Front Lights OFF
+      digitalWrite(PIN_LED, LOW);
+      break;
+    case 'U':  //Back ON
+      //digitalWrite(pinbackLights, HIGH);
+      break;
+    case 'u':  //Back OFF
+      //digitalWrite(pinbackLights, LOW);
+      break;
+    case 'D':  //Everything OFF
+      digitalWrite(PIN_LED, LOW);
+      //digitalWrite(pinbackLights, LOW);
+      motorStop();
+      break;        
+    default:  //Get velocity
+      if(command=='q'){
+        velocity = 255;  //Full velocity
+        //yellowCar.SetSpeed_4W(velocity);
+      }
+      else{
+        //Chars '0' - '9' have an integer equivalence of 48 - 57, accordingly.
+        if((command >= 48) && (command <= 57)){
+          //Subtracting 48 changes the range from 48-57 to 0-9.
+          //Multiplying by 25 changes the range from 0-9 to 0-225.
+          velocity = (command - 48)*25;      
+          //yellowCar.SetSpeed_4W(velocity);
+        }
+      }
+    }
+  }
+}
